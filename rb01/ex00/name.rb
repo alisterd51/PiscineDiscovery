83b59af597028b71ac0b1fb0e4429fe4afddb529
anclarma <anclarma@student.42.fr>
2021-01-17 15:13:06 +0100

    fin rb01

---
+++ rb01/ex00/name.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/ruby
+first_name = "Antoine"
+last_name = "Clarman"
+print first_name + " " + last_name + "\n"
---
+++ rb01/ex02/age.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/ruby
+my_age = 23 + 42
+print my_age.to_s + "\n"
